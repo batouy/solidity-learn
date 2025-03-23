46b59dadb3647d727804c5df3cc1d37a2e6a4271
batouy <524254210@qq.com>
2025-03-23 23:59:38 +0800

    array

---
+++ ByExample/Array.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Array {
+    uint256[] public arr;
+    uint256[] public arr2 = [1, 2, 3];
+    // Fixed sized array, all elements initialize to 0
+    uint256[10] public myFixedSizeArr;
+
+    function get(uint256 i) public view returns (uint256) {
+        return arr[i];
+    }
+}
